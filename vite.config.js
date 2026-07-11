0fd580987019a1e22a6a468e900931e3be68ae99
abukafa <hikam.abukafa@gmail.com>
2026-07-11 16:53:10 +0700

    change icons

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: [
         "favicon.svg",
-        "icons/icon-192x192.png",
-        "icons/icon-512x512.png",
+        "icons/icon-192x192-v2.png",
+        "icons/icon-512x512-v2.png",
       ],
       manifest: {
         name: "Bullet Journal",
@@ -22,19 +22,19 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "icons/icon-192x192.png",
+            src: "icons/icon-192x192-v2.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "icons/icon-512x512.png",
+            src: "icons/icon-512x512-v2.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "/icons/icon-512x512-maskable.png",
+            src: "/icons/icon-512x512-maskable-v2.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
